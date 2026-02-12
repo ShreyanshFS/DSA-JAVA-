8cc4809466d348a4716878693ef34d97bd57a4e4
ShreyanshFS <Shreyanshdwivedi15@gmail.com>
2026-02-12 19:08:24 +0530

    fixed

---
+++ Day1/basics.java
@@ -0,0 +1,7 @@
+public class basics{
+    public static void main(String args[]){
+        System.out.println("Hi");
+        System.out.print("Paaji\n");
+        System.out.print("Balle BAlle"); 
+    }
+}
