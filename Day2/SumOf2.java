91770613a7e02b7da2d1f560b7bf6bc93922229e
ShreyanshFS <Shreyanshdwivedi15@gmail.com>
2026-02-17 20:56:53 +0530

    FIle2

---
+++ Day2/SumOf2.java
@@ -0,0 +1,9 @@
+public class sumOf2 {
+    public static void main(String[] args) {
+        int a=5;
+        int b = 12;
+        int s ;
+        s = a+ b;
+        System.out.println(s);
+    }
+}
