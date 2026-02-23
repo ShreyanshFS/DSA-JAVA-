f0e6b72c1363b1a6342ded501cbbbeeedb088aaa
ShreyanshFS <Shreyanshdwivedi15@gmail.com>
2026-02-23 09:41:39 +0530

    Day3

---
+++ Day3/AreaOfcir.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class AreaOfcir {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float rad = sc.nextFloat();
+        float a = 3.14f*rad*rad;
+        System.out.println(a);
+    }
+    
+}
