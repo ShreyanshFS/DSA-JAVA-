d2cf5f32ec004e5d3b51c10678ac4b600f4692ed
ShreyanshFS <Shreyanshdwivedi15@gmail.com>
2026-03-04 16:41:45 +0530

    Day5,Project2

---
+++ Day5/calc.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class calc {
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of a");
+        int a = sc.nextInt();
+        System.out.println("Enter the value of b");
+        int b = sc.nextInt();
+        System.out.println("Enter the value of Operator Sign");
+        char op= sc.next().charAt(0); 
+
+        switch (op){
+            case '+': System.out.println(a+b);
+                        break;
+            case '-':System.out.println(a-b);
+                        break;
+            case '*':System.out.println(a*b);
+                        break;
+            case '/': System.out.println(a/b);
+                        break;
+            default:System.out.println("Error");
+        }
+    }    
+}
