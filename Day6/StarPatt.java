f7443c9a4946d8fcf9ed176308399e17d1829d76
ShreyanshFS <Shreyanshdwivedi15@gmail.com>
2026-03-09 21:13:04 +0530

    Day6 P7&8

---
+++ Day6/StarPatt.java
@@ -0,0 +1,11 @@
+
+public class StarPatt {
+    public static void main(String arg[]){
+        for(int l=1;l<11;l++){
+            for(int t=1;t<=l;t++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}  
---
+++ Day6/invStarPat.java
@@ -0,0 +1,10 @@
+public class invStarPat {
+    public static void main(String arg[]){
+        for(int l=10;l>0;l--){
+            for(int t=l;t>0;t--){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
