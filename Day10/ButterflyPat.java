1d273aa0ac6a6988e9a145e817c2268192f8d7be
ShreyanshFS <Shreyanshdwivedi15@gmail.com>
2026-03-29 09:36:21 +0530

    P4

--- Day10/ButterflyPat.java
+++ Day10/ButterflyPat.java
@@ -1,6 +1,37 @@
-inmport java.util.*;
+import java.util.Scanner;
 
 public class ButterflyPat {
-    public static void p(){}
-    public static void main (String[] args){}
+    public static void p(int n){
+        for (int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for (int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for (int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main (String[] args){
+        System.out.println("Enter n");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        p(n);
+    }
 }
