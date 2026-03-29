bfe19b80c97f739575447596e245b1693730d492
ShreyanshFS <Shreyanshdwivedi15@gmail.com>
2026-03-29 12:06:24 +0530

    array_agye_oye

---
+++ Day10/operationOnArray.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class operationOnArray {
+    public static void main(String[] args){
+        int marks[] = new int[50];//creatin'
+
+        Scanner sc = new Scanner(System.in);
+        marks[0] = sc.nextInt();
+
+        marks[0]=55;
+        System.out.println(marks[0]);
+        System.out.print(marks.length);
+    }
+}
