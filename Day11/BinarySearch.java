3d2816574d88bd75013d813543fcd1735a4608b3
ShreyanshFS <Shreyanshdwivedi15@gmail.com>
2026-03-30 10:33:23 +0530

    BinarySearch

--- Day11/BinarySearch.java
+++ Day11/BinarySearch.java
@@ -1,5 +1,38 @@
 import java.util.*;
 
 public class BinarySearch {
-    
+    public static int bs(int array[],int k){
+        int start =0,end = array.length-1;
+
+        while(start<=end){
+            int mid = (start+end)/2;
+
+            if(array[mid]==k)
+                return mid;
+            if (array[mid]<k)
+                start= mid+1;
+            else
+                end = mid -1;
+        }
+        return -1;
+    }
+    public static void main(String args[]){
+
+        System.out.println("Enter the no. of sub,marks and the mun to be found :");        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        int array[]= new int[s];
+        for (int i=0;i<array.length;i++){
+            array[i]=sc.nextInt();
+        }
+        int k = sc.nextInt();
+        sc.close();
+
+        int in = bs(array, k);
+        if (in == -1){
+            System.out.println("Not Found");
+        }
+        else{
+            System.out.println("Index of That num is " + in );
+        }
+    }
 }
