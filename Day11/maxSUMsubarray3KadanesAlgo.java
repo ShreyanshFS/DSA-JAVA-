1b8e46dfc787988bac5c583f5c07b92ad8ed46cb
ShreyanshFS <Shreyanshdwivedi15@gmail.com>
2026-04-10 16:59:17 +0530

    new_things

--- Day11/maxSUMsubarray3KadanesAlgo.java
+++
@@ -1,3 +0,0 @@
-public class maxSUMsubarray3KadanesAlgo {
-    
-}
--- Day12/maxSUMsubarray3KadanesAlgo.java
+++ Day12/maxSUMsubarray3KadanesAlgo.java
@@ -15,7 +15,8 @@ public class maxSUMsubarray3KadanesAlgo {
     }
     public static void main(String args[]){
 
-        System.out.println("Enter the no. of sub,marks and the mun to be found :");        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no. of sub,marks and the mun to be found :");       
+        Scanner sc = new Scanner(System.in);
         int s = sc.nextInt();
         int array[]= new int[s];
         for (int i=0;i<array.length;i++){
