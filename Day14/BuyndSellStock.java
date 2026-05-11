e54ce3cd4623503093afeff0d117b0dace28de18
ShreyanshFS <Shreyanshdwivedi15@gmail.com>
2026-05-11 15:57:18 +0530

    Updates

--- Day14/BuyndSellStock.java
+++ Day14/BuyndSellStock.java
@@ -1,3 +1,20 @@
 public class BuyndSellStock {
-    
+    public static int BnSstock(int cost[]){
+        int bp =Integer.MAX_VALUE;
+        int mpt=0;
+         
+        for (int i=0;i<cost.length;i++){
+            if (bp<cost[i]){
+                int p=cost[i]-bp;
+                mpt=Math.max(mpt, p);
+            } else{
+                bp = cost[i];
+            }
+        }
+        return mpt;
+    }
+    public static void main(String[] args){
+        int cost[] = {7,1,5,3,6,4};
+        System.out.println("Profit "+BnSstock(cost));
+    }
 }
